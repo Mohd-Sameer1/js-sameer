19838ae7b71a71d29bb980d6c22c56d047f57027
Mohd-Sameer1 <147045127+Mohd-Sameer1@users.noreply.github.com>
2023-10-06 13:26:02 +0000

    done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -5,12 +5,15 @@ let accountEmail = "mohdabdulsameer1633@gmail.com"
 var accountPassword = "12345"
 accountCity = "Hyderabad"
 
+//accountId = 2// not allowed 
 
 accountEmail = "sameer@gmail.com"
 accountPassword = "2121111"
 accountCity = "Delhi"
-accountState;
+let accountState;
+
+/* preferred not to use Var because of issue in block scope and functional scope   */
 
 console.log(accountId);
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
 //Datatypes//
