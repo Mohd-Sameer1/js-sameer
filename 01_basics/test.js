f47e0c43256211af5390907433664e4e2169c27a
Mohd-Sameer1 <147045127+Mohd-Sameer1@users.noreply.github.com>
2023-10-06 13:08:24 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,10 +1,16 @@
 console.log("sameer")
-
+//variables//
 const accountId = 144553
 let accountEmail = "mohdabdulsameer1633@gmail.com"
 var accountPassword = "12345"
 accountCity = "Hyderabad"
 
 
+accountEmail = "sameer@gmail.com"
+accountPassword = "2121111"
+accountCity = "Delhi"
+accountState;
 
 console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//Datatypes//
