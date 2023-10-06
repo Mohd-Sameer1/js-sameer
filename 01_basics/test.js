415d43eec7afdbc0adc26cd1a4a947a6868c9bb0
Mohd-Sameer1 <147045127+Mohd-Sameer1@users.noreply.github.com>
2023-10-06 12:48:35 +0000

    container config done

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,10 @@
 console.log("sameer")
+
+const accountId = 144553
+let accountEmail = "mohdabdulsameer1633@gmail.com"
+var accountPassword = "12345"
+accountCity = "Hyderabad"
+
+
+
+console.log(accountId);
